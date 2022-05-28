5fda8b2db58b5734daed22cb8e06015f0d6f9093
Paul Butler <paul@driftingin.space>
2022-05-28 13:01:47 +0000

    initial check-in

---
+++ src/test-spawn.ts
@@ -0,0 +1,14 @@
+import test from 'ava'
+
+test('ok', async (t) => {
+    t.pass("ok")
+})
+
+test('foobar', async (t) => {
+    const foo_ = foobar()
+    const foo_5 = foo()
+    const foo_6 = foo()
+    const foo_7 = foo()
+    const foo_8 = foo()
+    const foo_9 = foo()
+})
